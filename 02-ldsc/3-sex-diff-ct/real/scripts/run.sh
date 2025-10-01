6738118410d75532c5f934e198a9ca1b30d012f0
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-10-01 14:53:06 +0200

    Update and rename job.sh to run.sh

--- 02-ldsc/3-sex-diff-ct/real/scripts/run.sh
+++ 02-ldsc/3-sex-diff-ct/real/scripts/run.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH -p haswell # partition (queue)
 #SBATCH -N 1 # number of nodes
-#SBATCH -J ct-heatmap
+#SBATCH -J ct-diff-tab-heatmap
 #SBATCH --mem 60G # memory pool for all cores
 #SBATCH -t 0-02:00 # time (D-HH:MM)
 #SBATCH -o ./real/outputs/log.%j.out # STDOUT
@@ -11,13 +11,13 @@
 #SBATCH --mail-user=claudia.vasallo@upf.edu # send-to address
 
 # Modules
-module load  R
-module load  R/4.2.0-foss-2021b
+module purge
+module load modulepath/haswell
+module load R/4.3.2-gfbf-2023a
 
-
+# Config
 RESULT=$1
+POWER=$2
+META=$3
 
-
-#Rscript ./real/scripts/crosstrait_diff.R  ${RESULT}
-
-Rscript real/scripts/heatmap.rg.intrasex.R
+Rscript ./real/scripts/crosstrait_diff.R  ${RESULT} ${POWER} ${META}
