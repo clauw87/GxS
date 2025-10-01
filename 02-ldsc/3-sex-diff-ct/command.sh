b336cb30775c3bd1f9a9c66d2c45eb9c21c2df2b
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-10-01 14:48:51 +0200

    Update command.sh

--- 02-ldsc/3-sex-diff-ct/command.sh
+++ 02-ldsc/3-sex-diff-ct/command.sh
@@ -1,16 +1,65 @@
+#!/bin/bash
 
+# Main Code --------------------------------------------------------
+
+main(){
+  
+# Creates and cleans directory structure
+clean_directory
+
+# Configuration
 RES=../3-join_rg_results/real/outputs/formatted_rg_res.txt
+POWERED=../2-join_h2_results/real/outputs/h2_powered_4.txt
+METAFILE=../../00-download/1-get_traitsinfo/real/outputs/joined_metadata_domains.txt
 
 
-#module load R 
-#module load  R/4.2.0-foss-2021b
-
-
-
-COMMAND="./real/scripts/job.sh ${RES}"
+COMMAND="./real/scripts/job.sh ${RES} ${POWERED} ${METAFILE}"
 
 sbatch ${COMMAND}
+     
+} 
 
 
-#Rscript ./real/scripts/crosstrait_diff.R  ${RES}
-#Rscript ./real/scripts/rg-diff1-barplot.R
+# FUNCTIONS ==========================================================
+
+
+clean_directory(){
+  # Creates and cleans directory structure
+  
+  if [ ! -d "real" ]; then
+
+    mkdir real
+    mkdir real/scripts
+    mkdir real/inputs
+    mkdir real/tmp
+    mkdir real/outputs
+
+  else
+
+    if [ ! -d "real/scripts" ]; then
+  mkdir real/scrips
+    fi
+
+    if [ ! -d "real/inputs" ]; then
+  mkdir real/inputs
+    fi
+
+    if [ ! -d "real/tmp" ]; then
+  mkdir real/tmp
+    else
+  rm -fR real/tmp/*
+    fi
+
+    if [ ! -d "real/outputs" ]; then
+  mkdir real/outputs
+    else
+  rm -fR real/outputs/*
+    fi
+
+  fi
+}
+ 
+ 
+main
+
+
