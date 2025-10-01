f8d6dee914ac4a86bd3d0c828e91900948154b87
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-10-01 13:26:49 +0200

    Update run.sh

--- 02-ldsc/2-sex-diff-h2/real/scripts/run.sh
+++ 02-ldsc/2-sex-diff-h2/real/scripts/run.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=haswell
+#SBATCH --job-name=h2_res_join
+#SBATCH --output=./real/outputs/out.out
+#SBATCH --error=./real/outputs/err.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=16GB
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=END
+#SBATCH --mail-user=claudia.vasallo@upf.edu
 
+
+module purge
+module load modulepath/haswell
+module load R/4.3.2-gfbf-2023a
+
+
+# Config
+POWERED_FILE=$1
+METADATA=$2
+OUTPUT_DIR=$3 
+
+H2_FILE=../../01d-power-calc/real/outputs/h2_liab.txt
+cat ${POWERED_FILE}  > ./real/inputs/target.txt
+TARGET=./real/inputs/target.txt
+
+Rscript ./real/scripts/compare_h2.R ${H2_FILE} ${TARGET} ${OUTPUTS_DIR}
+
+Rscript ./real/scripts/h2_plot_table.R ./real/outputs/compare_h2_liab_scale.txt
