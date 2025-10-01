85c0ec74260aaebb78facea5147a90fd2048eb69
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-10-01 14:43:39 +0200

    Update run.sh

--- 02-ldsc/3-join_rg_results/real/run.sh
+++ 02-ldsc/3-join_rg_results/real/run.sh
@@ -24,21 +24,20 @@ main(){
   echo -e 'Trait\tIntercept\tIntercept_SE\tGC\tGC_SE\tPValue' > ${RES_FILE}
   for FILE in ../3-rg/real/outputs/*-genetic-correlation.log
   do
-    TRAIT1=$(basename ${FILE} | cut -d'.' -f1)
-    TRAIT2=$(basename ${FILE} | cut -d'.' -f2  | awk '{gsub(/-genetic-correlation/, ""); print}')
+    NAME=$(echo ${FILE} | sed 's/-0.0./-0-0./g') # problematic names in Berbabeu 2021 ids
+    TRAIT1N=$(basename ${NAME} | cut -d'.' -f1)
+    TRAIT2N=$(basename ${NAME} | cut -d'.' -f2  | awk '{gsub(/-genetic-correlation/, ""); print}')
+    TRAIT1=$(echo ${TRAIT1N} | sed 's/-0-0/-0.0/g')
+    TRAIT2=$(echo ${TRAIT2N} | sed 's/-0-0/-0.0/g')
     INTERCEPT_LINE=$(cat ${FILE} | grep -i -A 6 'Heritability of phenotype 2/2' | grep -i intercept)
     INTERCEPT=$(echo ${INTERCEPT_LINE} | cut -d" " -f2)
     INTERCEPT_SE=$(echo ${INTERCEPT_LINE} | cut -d" " -f3 | tr -d "(" | tr -d ")")
-
     GC_LINE=$(cat ${FILE} |  grep -A 4 -i 'genetic correlation$' | grep -i 'genetic correlation:')
     GC=$(echo ${GC_LINE} | cut -d" " -f3 )
     GC_SE=$(echo ${GC_LINE} | cut -d" " -f4 | tr -d "(" | tr -d ")")
-
     P_LINE=$(cat ${FILE} |  grep -A 4 -i 'genetic correlation$' | grep -i 'P:')
     P=$(echo ${P_LINE} | cut -d":" -f2 )
-
     echo -e "${TRAIT1}\t${TRAIT2}\t${INTERCEPT}\t${INTERCEPT_SE}\t${GC}\t${GC_SE}\t${P}" >> ${RES_FILE}
-
   done
 
 
