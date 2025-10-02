4db1909361134f0b9ca64a82e0c7ca2e650a3587
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-10-02 14:31:13 +0200

    Create run.sh

---
+++ 02-ldsc/3-sex-diff-rg1/real/scripts/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH -p haswell # haswell (queue)
+#SBATCH -N 1 # number of nodes
+#SBATCH --job-name=rg_diff1
+#SBATCH --output=./real/outputs/out.out
+#SBATCH --error=./real/outputs/err.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=200GB
+#SBATCH --mail-type=END
+#SBATCH --mail-user=claudia.vasallo@upf.edu
+
+
+module purge
+module load modulepath/haswell
+module load R/4.3.2-gfbf-2023a
+
+
+# CONFIG
+RES=$1
+
