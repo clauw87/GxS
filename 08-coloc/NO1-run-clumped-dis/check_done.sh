d6b0db716b1d7dd79dc28260791bafca026b77dd
Claudia Vasallo Vega <claudiaw87@gmail.com>
2025-11-04 15:23:23 +0100

    Delete 08-coloc/NO1-run-clumped-dis directory

--- 08-coloc/NO1-run-clumped-dis/check_done.sh
+++
@@ -1,7 +0,0 @@
-cat real/inputs/indep.loci  | xargs -I {} basename {}  | cut -d '.' -f1 > target.codes
-
-#ls real/outputs_1203/coloc_*.txt
-
-ls real/outputs/coloc_*.txt | xargs -I {} basename {} | sed  's/coloc_//g' | cut -d '.' -f1  > done.codes
-
-cat target.codes | grep -v -f done.codes  > todo.codes
