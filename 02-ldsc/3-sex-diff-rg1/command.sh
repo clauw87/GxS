1972b588887c3da94e29411a5e7f945e7fdcca16
Claudia Vasallo Vega <claudiaw87@gmail.com>
2026-02-18 13:42:23 +0100

    Add files via upload

--- 02-ldsc/3-sex-diff-rg1/command.sh
+++ 02-ldsc/3-sex-diff-rg1/command.sh
@@ -3,32 +3,34 @@
 # Main Code --------------------------------------------------------
 
 main(){
-  
+
  # Creates and cleans directory structure
- clean_directory
+ #clean_directory
 
 
 
   # Configuration
-  RES=../3-join_rg_results/real/outputs/
-
-
-
-
-
+  RES_FILE=../3-join_rg_results/real/outputs/formatted_rg_res.txt
+  #RES_FILE=../3-join_rg_results/real/outputs/formatted_rg_res_requested.txt
+  META_FILE=../../00-download/1-get_traitsinfo/real/outputs/joined_metadata_domains_updated.txt
+  FIL=True # remove Bernabeu ones for rg1 -- , and Neales
+  OUTPUTS_DIR=./real/outputs
 
   # COMMAND
   COMMAND=" \
     ./real/scripts/run.sh \
-        ${RES_FILE} 
+        ${RES_FILE} \
+        ${META_FILE} \
+        ${FIL} \
+        ${OUTPUTS_DIR}
   "
-          
-   
+
+
   sbatch ${COMMAND}
   exit
 
-     
-} 
+
+}
 
 
 
