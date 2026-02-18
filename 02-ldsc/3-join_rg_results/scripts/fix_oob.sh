4d8631c8e5707b65f6fdeb742293a90d6e28bba9
Claudia Vasallo Vega <claudiaw87@gmail.com>
2026-02-18 13:39:26 +0100

    Delete 02-ldsc/3-join_rg_results/scripts directory

--- 02-ldsc/3-join_rg_results/scripts/fix_oob.sh
+++
@@ -1,8 +0,0 @@
-# Create table with RG values
-# header
-cat real/tmp/r9m:r9m | head -n1 > ./real/outputs/genetic-correlations.txt
-# results line
-cat real/tmp/* | grep -v -w gcov_int >> ./real/outputs/genetic-correlations.txt
-
-#   cat ./real/outputs/genetic-correlations_oob.txt | wc -l  # 13689
-#   cat ./real/outputs/genetic-correlations.txt | wc -l  #  13689
