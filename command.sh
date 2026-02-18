64c11b9855de7e3c67adbb092af530946c19ff6c
Claudia Vasallo Vega <claudiaw87@gmail.com>
2026-02-18 13:35:12 +0100

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Main Code --------------------------------------------------------
+
+main(){
+
+  # Creates and cleans directory structure
+  clean_directory
+
+
+  # Configuration
+
+  GR_OUTPUTS_FOLDER=../3-rg/real/outputs
+  META_FILE=../../00-download/1-get_traitsinfo/real/outputs/joined_metadata_domains_updated.txt
+  POWERED_FILE=../2-join_h2_results/real/outputs/h2_powered_2.txt
+
+  # COMMAND
+  COMMAND=" \
+    ./real/scripts/run.sh \
+        ${GR_OUTPUTS_FOLDER} \
+        ${META_FILE} \
+        ${POWERED_FILE}
+  "
+
+
+  sbatch ${COMMAND}
+  exit
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# FUNCTIONS ==========================================================
+
+
+clean_directory(){
+  # Creates and cleans directory structure
+  
+  if [ ! -d "real" ]; then
+
+    mkdir real
+    mkdir real/scripts
+    mkdir real/inputs
+    mkdir real/tmp
+    mkdir real/outputs
+
+  else
+
+    if [ ! -d "real/scripts" ]; then
+  mkdir real/scripts
+    fi
+
+    if [ ! -d "real/inputs" ]; then
+  mkdir real/inputs
+    fi
+
+    if [ ! -d "real/tmp" ]; then
+  mkdir real/tmp
+    else
+  rm -fR real/tmp/*
+    fi
+
+    if [ ! -d "real/outputs" ]; then
+  mkdir real/outputs
+    else
+  rm -fR real/outputs/*
+    fi
+
+  fi
+}
+ 
+ 
+main
